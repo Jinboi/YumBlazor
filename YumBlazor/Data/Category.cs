89d63bd907929b43f4d35b8c3824a16c8a8530f2
Jinboi <duddnwkfgo7@gmail.com>
2024-11-06 21:13:19 -0800

    Added loader gif, finished 90.

--- YumBlazor/Data/Category.cs
+++ YumBlazor/Data/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace YumBlazor.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
 {
     public class Category
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name..")]
+        public string Name { get; set; }
 
     }
 }
