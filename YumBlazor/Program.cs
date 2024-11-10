2ba3c971c00220ee3fde0ab04dbf72ac4b2ecbd1
Jinboi <duddnwkfgo7@gmail.com>
2024-11-10 10:43:34 -0800

    re-pushing

--- YumBlazor/Program.cs
+++ YumBlazor/Program.cs
@@ -9,7 +9,7 @@ using YumBlazor.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container..
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
